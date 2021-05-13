12df5d090369faac0eb5d085b320b1950f256e39
nikita1803 <rainikita595@gmail.com>
2021-05-13 13:28:09 +0530

    Added:Calculated randome dice number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome in snake and ladder simulation");
 		int start_position_player1 = 0 ;
+		int dice = (int) ((Math.random() * 10) % 6 + 1);
+		System.out.println(dice);
 		
 	}
 
